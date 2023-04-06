c0bb9b791dd2fc885340cd23f26fc02fd6f71775
jjhbotache <jjhuertasbotache@gmail.com>
2023-04-06 16:04:44 -0500

    CRUD designs endede

--- back/api_rest.py
+++ back/api_rest.py
@@ -1,10 +1,11 @@
 from flask import Flask,jsonify,request,send_file
 import pymysql
 import os
+import re
 from flask_cors import CORS
 app = Flask(__name__)
-
 CORS(app)
+
 
 @app.route('/')
 def test():
@@ -21,89 +22,185 @@ class Data_base:
         )
         self.cursor = self.connection.cursor()
     
-    def get_msgs(self):
-        self.cursor.execute(f"""
-                            SELECT name,png FROM designs
-                            """)
+    # def get_msgs(self):
+    #     self.cursor.execute(f"""
+    #                         SELECT name,png FROM designs
+    #                         """)
         
-        return self.cursor.fetchall()
+    #     return self.cursor.fetchall()
     
-    def insert_design(self,msg,img_dir):
-        self.cursor.execute(f"""
-                            INSERT INTO designs(name,png)
-                            VALUES(
-                                "{msg}",
-                                "{img_dir}"
+    # def insert_design(self,msg,img_dir):
+    #     self.cursor.execute(f"""
+    #                         INSERT INTO designs(name,png)
+    #                         VALUES(
+    #                             "{msg}",
+    #                             "{img_dir}"
+    #                             )
+    #                         """)
+    #     self.connection.commit()
+
+    # def delete_msg(self,id):
+    #     self.cursor.execute(f"""
+    #                         DELETE FROM messages
+    #                         WHERE id = {id}
+    #                         """)
+    #     self.connection.commit()
+        
+    # def update_msg(self,id,msg):
+        # self.cursor.execute(f"""
+        #                     UPDATE messages
+        #                     SET msg = "{msg}"
+        #                     WHERE id = {id}
+        #                     """)
+        # self.connection.commit()
+        
+    def save_design(self,name,img_route,ai_route):
+        try:
+            self.cursor.execute(f"""
+                                INSERT INTO designs(name,img,ai)
+                                VALUES(
+                                    "{name}",
+                                    "{img_route}",
+                                    "{ai_route}"
                                 )
-                            """)
-        self.connection.commit()
-
-    def delete_msg(self,id):
-        self.cursor.execute(f"""
-                            DELETE FROM messages
-                            WHERE id = {id}
-                            """)
-        self.connection.commit()
+                                """)
+            self.connection.commit()
+            return True
+        except Exception as e:
+          print('An exception occurred: ',e)
+          return False
         
-    def update_msg(self,id,msg):
+    def get_all_designs(self):
+        try:
+            self.cursor.execute(f"""
+                                SELECT name,img,ai,id_designs FROM designs
+                                """)
+            data = self.cursor.fetchall()
+            print(data)
+            formated_data = []
+            os.system("cls")
+            for row in data:
+                formated_data.append(
+                    {
+                        "name":row[0],
+                        "img_url":row[1],
+                        "ai_url":row[2],
+                        "id": row[3],
+                    }
+                )
+            return formated_data
+        except Exception as e:
+            print('An exception occurred: ',e)
+            return False
+      
+    def update_design(self,id,field,new_data):
+        assert field in ["name","img","ai"]
         self.cursor.execute(f"""
-                            UPDATE messages
-                            SET msg = "{msg}"
-                            WHERE id = {id}
+                            UPDATE designs
+                            SET {field} = "{new_data}"
+                            WHERE id_designs = {id}
                             """)
         self.connection.commit()
-        
 
-
+    def delete_design(self,id):
+        self.cursor.execute(f"""
+                            DELETE FROM DESIGNS
+                            WHERE id_designs = {id}
+                            """)
+        self.connection.commit()
 # ----------------------------------------------------------------------------------------
-@app.route("/msg")
-def show_msng():
-    os.system("cls") #clean console
-    db = Data_base() #connect
-    data = db.get_msgs() #getting the data
-    to_send = {
-        "src":f"http://localhost:1000/img/{data[0][0]}"
-    }
-        
-    return jsonify(to_send)    #return the data
-
-@app.route("/img/<string:name>")
-def get_img(name):
-    return send_file(f"imgs/Que_tu_niña_interior_viva_orgullosa_del_mujeron_que_eres.png", mimetype='image/png')
-
-@app.route("/msg",methods=["POST"])
-def send_msg():
-    os.system("cls")
-    msg = request.form["msg"]
-    print("~"*50)
-    print(msg)
-    print("~"*50)
+@app.route("/design", methods=["POST"])
+def save_img():
+    name = request.form["name"]
+    files_name = request.form["filesName"]
     img = request.files["img"]
-    route = "imgs/"+msg
-    img.save(route)
+    ai = request.files["ai"]
+    print("name: ",name)
+
+    route_img = f"img/{files_name}.png"
+    route_ai = f"ai/{files_name}.ai"
+
+    if os.path.isfile(route_ai) or os.path.isfile(route_img):
+        return f"file with name {name} already exist"
+    else:
+        try:
+            img.save(route_img)
+            ai.save(route_ai)
+            connection = Data_base()
+            connection.save_design(
+                name,
+                route_img,
+                route_ai
+            )
+            return "Saved succesfully"
+        except Exception as e:
+            return f"Something went wrong {e}"
+
+@app.route("/design", methods=["GET"])
+def get_designs():
+    connection = Data_base()
+    return jsonify(connection.get_all_designs())
+
+@app.route("/<string:kind>/<string:name>")
+def get_file(kind,name):
+    route = f"{kind}/{name}"
+    if kind == "img":
+        return send_file(route, mimetype='image/png')
+    elif kind == "ai":
+        return send_file(route)
+
+@app.route("/update_design/<int:id>/<string:field>", methods=["POST"])
+def update(id,field):
+    try:
+        if field == "name":
+            data = request.get_json()["new_data"]
+            conn = Data_base()
+            conn.update_design(id,field,data)
+            
+        if field == "img":
+            print("changing img")
+            conn = Data_base()
+            designs = conn.get_all_designs()
+            route_img = list(filter(lambda design : design["id"]==id, designs))[0]["img_url"]
+            print("changing the img at ",route_img)
+            
+            img = request.files["img"]
+            os.remove(route_img)
+            img.save(route_img)
+        
+        if field == "ai":
+            print("changing img")
+            conn = Data_base()
+            designs = conn.get_all_designs()
+            route_ai = list(filter(lambda design : design["id"]==id, designs))[0]["ai_url"]
+            ai = request.files["ai"]
+            os.remove(route_ai)
+            ai.save(route_ai)
+        
+        return jsonify({"msg":"updated succesfully"})
     
-
-    db=Data_base()
-    db.insert_design(msg,route)
-    return f"saved in {route}"
-
-@app.route("/msg/<int:id>",methods=["DELETE"])
-def delete_msg(id):
-    os.system("cls")
-    db=Data_base()
-    db.delete_msg(id)
-    print(db.get_msgs())
-    return "done"
-
-@app.route("/msg/<int:id>",methods=["PUT"])
-def update_msg(id):
-    os.system("cls")
-    sended_msg = request.json["msg"]
-    db=Data_base()
-    db.update_msg(id,sended_msg)
-    print(db.get_msgs())
-    return "done"
-
+    except Exception as e:
+        return jsonify({"msg":f"An exception occurred: {e}"})
+    
+@app.route("/delete_design/<int:id>", methods=["DELETE"])
+def delete_design(id):
+    try:
+        print(f"DELETING design with id: {id}")
+        conn = Data_base()
+        designs = conn.get_all_designs()
+        design = list(filter(lambda design : design["id"]==id, designs))[0]
+        route_ai=design["ai_url"]
+        route_img=design["img_url"]
+        os.remove(route_ai)
+        os.remove(route_img)
+        conn.delete_design(id)
+        
+        return jsonify({"msg":"deleted succesfully"})
+    
+    except Exception as e:
+        return jsonify({"msg":f"An exception occurred: {e}"})
+    
+      
 # ----------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
--- front/main.js
+++ front/main.js
@@ -1,157 +1,273 @@
-const btnSave = document.getElementById("saveBtn");
-const btnGet = document.getElementById("getBtn");
-const btnDelete = document.getElementById("deleteBtn");
-const btnUpdate = document.getElementById("updateBtn");
-const input = document.getElementById("input");
+const btnSave = document.getElementById("btn-save");
+const btnUpdate = document.getElementById("btn-update");
+const inputName = document.getElementById("input-name");
+const inputImg = document.getElementById("input-img");
+const inputAi= document.getElementById("input-ai");
 const preview = document.getElementById("preview")
-const input_img = document.getElementById("input-img");
-const label = document.getElementById("label");
-const select = document.getElementById("delete");
+const realName = document.getElementById("real-name");
 
 
-let templateContent;
-// import & create template
-fetch("templates/msg.html")
-.then(response=>response.text())
-.then(text=>{
-  // creamos un elemento template
-  const template = document.createElement("template");
-  // al que le damos un id
-  template.id = "msg-template";
+const select = document.getElementById('select-designs');
 
-  //agregamos la data obtenida de un html externo al template 
-  template.innerHTML = text;
-  // agregamos el elemento al documento
-  document.body.appendChild(template);
-  // redefinimos a templatecontent como el contenido del template
+// -------------
+let fragment = document.createDocumentFragment()
+const viewContainer = document.getElementById("view-container");
+const designs = [];
+const designViewTemplate = document.createElement("template");
+// -------------
+
+btnSave.addEventListener("click",()=>{
+  if (inputName.value==""){
+    alert("Please enter a name");
+  }else if (inputImg.files[0].lenght>0) {
+    alert("Please enter an Img");
+  }else if (inputAi.files[0].lenght>0) {
+    alert("Please enter an Ai");
+  }else{
+    sendDesign(inputName.value,realName.textContent, inputImg.files[0], inputAi.files[0]);
+  }
+})
+inputName.addEventListener("input",()=>{
+  updateRealName();
+});
+inputImg.addEventListener('change',()=>{
+  inputName.value = inputImg.files[0].name;
+  const reader = new FileReader();
+  reader.onloadend = () => {preview.src = reader.result};
+  reader.readAsDataURL(inputImg.files[0])
+
+  preview.src = inputImg.files[0]
+  updateRealName();
 });
 
-const msgContainer = document.getElementById("msg-container");
-let msgs = [
-  // [0,"hola"],
-  // [1,"adios"],
-  // [3,"como estas?"]
-];
+// =========================================================================================
 
-function getMsgs() {
-  msgContainer.innerHTML=""
+function sendDesign(name,filesName,img,ai) {
+  const data = new FormData();
+  data.append('name', name);
+  data.append('filesName', filesName);
+  data.append('img', img);
+  data.append('ai', ai);
+
+  console.log(`sending=`,data);  
   fetch(
-    "http://localhost:1000/msg"
-  )
-  .then(response => response.json())
-  .then(data => {
-    
-    const src = data["src"];
-    console.log(`src`, src);
-    preview.src=src;
-    
-    // msgs.forEach(msg => {
-    //   select.innerHTML+=`
-    //   <option value="${msg[0]}">${msg[1]}</option>
-    //   `;
-    // });
-    // renderMsgs();
-  })
-}
-
-function sendMsg(message,callback) {
-  const obj = new FormData();
-  obj.append('msg', message);
-  obj.append('img', input_img.files[0]);
-
-  console.log(`sending=`,obj);  
-  fetch(
-    "http://localhost:1000/msg",
+    "http://localhost:1000/design",
     {
       method:"POST",
-      // headers: {
-      //   'Content-Type': 'multipart/form-data'
-      // },
-      body:obj
+      body:data
     }
   )
   .then(respuesta=>respuesta.text())
   .then(data=>{
-    // callback();
     alert(data);
   })
   .catch(e=>{
     alert("somethig went wrong:",e);
   })
 }
-function deleteMsg(id,callback) {
-  console.log(`sending msg with id: ${id}`);
-  fetch(
-    `http://localhost:1000/msg/${id}`,
-    {
-      method:"DELETE",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    }
-  )
-  .then(respuesta=>respuesta.text())
-  .then(data=>{
-      callback();
-      alert(data);
+
+function loadDesigns() {
+  fetch('http://127.0.0.1:1000/design')
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById("designs").classList.remove("d-none")
+      document.getElementById("designs-spinner").classList.add("d-none")
+
+      console.log(data);
+      // start to make the objects from a template --------------------------------------------------------
+      data.forEach(design => {
+        designs.push(design)
+
+        // add options with a name
+        const option = document.createElement('option');
+        // option.id = removeAfterDot(design.name);
+        option.value = removeAfterDot(design.name);
+        option.textContent = design.name;
+        select.appendChild(option);
+        option.addEventListener('dblclick', (e) => {
+          loadAndShowDesign(option.value);
+        });
+      });
+      
     })
-  .catch(e=>{
-    alert("somethig went wrong");
-  })
-}
-function updateMsg(id,message,callback) {
-  console.log(`replace the msg with id: ${id} \n with the msg: `);
-  obj = {msg:message};
-  fetch(
-    `http://localhost:1000/msg/${id}`,
-    {
-      method:"PUT",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body:JSON.stringify(obj)
-    }
-  )
-  .then(respuesta=>respuesta.text())
-  .then(data=>{
-      callback();
-      alert(data);
-    })
-  .catch(e=>{
-    alert("somethig went wrong");
-  })
-  
+    .catch(error => {
+      console.error('Error:', error);
+      select.innerHTML="";
+      fragment.innerHTML="";
+      setTimeout(() => {
+        console.log("trying again...");
+        loadDesigns()
+      }, 1000);
+    });
 }
 
-btnGet.addEventListener("click",()=>{    getMsgs()                           ;})
-btnSave.addEventListener("click",()=>{   sendMsg(input.value,getMsgs)                ;})
-btnDelete.addEventListener("click",()=>{ deleteMsg(select.value,getMsgs)             ;})
-btnUpdate.addEventListener("click",()=>{ updateMsg(select.value,input.value,getMsgs) ;})
+function loadAndShowDesign(name) {
+  const namesList = Array.from(viewContainer.querySelectorAll(".floating-card")).map(view => view.id);
+  if (namesList.includes(name)) {
+    document.getElementById(name).classList.toggle("d-none");
+  }
+  else{
+    createView(name);
+    loadAndShowDesign(name)
+  }
+}
 
+function createView(name) {
+  const design = designs.find(design => design.name === name);
+  const newView = document.importNode(
+    designViewTemplate.content,
+    true
+  );
+  const apiRoute = "http://127.0.0.1:1000/";
+  const id = removeAfterDot(design.name);
+  const all = newView.querySelector(".floating-card");
+  all.id = id;
+  newView.getElementById("img").src = apiRoute + design.img_url;
+  newView.getElementById("download-ai").href = apiRoute + design.ai_url;
+  newView.getElementById("img-name").textContent = design.name;
 
-input_img.addEventListener("input",()=>{
-  const name = input_img.files[0].name;
-  input.value = name.split("-").join("_");
-})
+  newView.querySelector(".floating-card").addEventListener('click', (e)=>{
+    // console.log(e.target);
+    if (e.target.id === "bg") all.classList.add("d-none");
+    if (e.target.id === "update-img" ) {
+      updateDesign(design.name);
+      all.classList.add("d-none");
+    };
+    if (e.target.id === "delete") {
+      if (confirm(`Do you want do delete "${design.name}" design?`)) {
+        fetch(apiRoute+"delete_design/"+design.id.toString(),{method:"DELETE"})
+        .then((response) => {
+          response.json()
+        })
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((e) => {
+          console.log(e);
+        })
+      }
+    };
+  });
+  fragment.appendChild(newView);
+  viewContainer.appendChild(fragment);
+}
 
-
-
-
-function renderMsgs() {
-  msgs.forEach(msg => {
-    console.log(msg);
-    // creo un nuevo objeto, a partir del contenido del template
-    const newMsg = document.importNode(
-      document.getElementById("msg-template").content,
-      true);
-      
-      // modifico el nuevo obj
-      newMsg.getElementById("msg").textContent = msg[1];
-      
-      // lo anexo a un contenedor
-      msgContainer.appendChild(newMsg);
+function waitForUpdate(name) {
+  return new Promise(resolve => {
+    btnUpdate.addEventListener('click', () => {
+      if (confirm(`
+      Are you sure do you want to update ${name} to:\n
+      NAME:
+      ${inputName.value}
+      PNG:
+      ${inputImg.files.length == 0?"*The same*":inputImg.files[0].name}
+      AI:
+      ${inputAi.files.length == 0?"*The same*":inputAi.files[0].name}
+      `)) resolve('Botón presionado');
+    });
   });
 }
 
+async function updateDesign(name) {
+  const design = designs.find(design => design.name === name);
+  inputName.value = design.name
+  updateRealName()
+  preview.src = 'http://127.0.0.1:1000/'+design.img_url;
+ 
+  document.getElementById("title").textContent = "Edit your design"
+  btnSave.classList.add("d-none");
+  btnUpdate.classList.remove("d-none");
+  
+  await waitForUpdate(design.name);
+  
+  if (inputAi.files.length>0) {
+    console.log(`Changing ai file`);
+  
+    const data = new FormData();
+    data.append("ai",inputAi.files[0])
+  
+    fetch(`http://127.0.0.1:1000/update_design/${design.id}/ai`,
+    {
+      method: 'POST',
+      body:data
+    })
+    .then(response => response.json())
+    .then((data) => {console.log(data);})  
+    .catch(error => console.log(error));
+  }
+  if (inputImg.files.length>0) {
+    console.log(`Changing img file`);
+
+    const data = new FormData();
+    data.append("img",inputImg.files[0])
+
+    fetch(`http://127.0.0.1:1000/update_design/${design.id}/img`,
+    {
+      method: 'POST',
+      body:data
+    })
+    .then(response => response.json())
+    .then((data) => {console.log(data);})  
+    .catch(error => console.log(error));
+  }
+  if (inputName.value != design.name) {
+    console.log(`Changing name from ${design.name} to ${inputName.value}`);
+    fetch(`http://127.0.0.1:1000/update_design/${design.id}/name`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify(
+        {
+          new_data:inputName.value
+        }
+      )
+    })
+    .then(response => response.json())
+    .then((data) => {console.log(data);})  
+    .catch(error => console.log(error));
+  }
 
 
+  btnUpdate.classList.add("d-none");
+  btnSave.classList.remove("d-none");
+  document.getElementById("title").textContent = "Add your design"
+  
+  alert("updated!")
+  window.location.reload();
+}
+
+// =========================================================================================
+// --------------------------------------------------------------------------------------------------------------
+function removeAfterDot(str) {
+  const dotIndex = str.indexOf('.');
+  if (dotIndex === -1) return str;
+  return str.slice(0, dotIndex);
+}
+
+function updateRealName(str) {
+  str = str||inputName.value;
+  console.log(str);
+  if (inputName.value.includes(".")) {
+    document.getElementById("dot-alert").classList.remove("d-none");
+  }else{document.getElementById("dot-alert").classList.add("d-none");}
+
+  realName.textContent = (str.split(" ").join("_")).replace(/[\\/*<>| ]/g, "_");
+  realName.textContent = realName.textContent.replace(/[?:"]/g, "-");
+  realName.textContent = realName.textContent.substring(0,realName.textContent.indexOf(".")!=-1?realName.textContent.indexOf("."):realName.textContent.length)
+  
+}
+// -------------------------------------------------------------------------------------------------------------
+
+// creates a template
+fetch('templates/design-view.html')
+.then(response => response.text())
+.then(data => {
+    // console.log(data);
+    designViewTemplate.id         =   "design-view-template";
+    designViewTemplate.innerHTML  =   data;
+    document.body.appendChild(designViewTemplate);
+})
+
+loadDesigns();
